b649dccd2154b6a97616bed95d6f7868b0454fd8
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-02-05 15:32:38 +0000

    Update FactorialRegularInputTesting.java

--- src/main/java/com/epam/rd/autotasks/FactorialRegularInputTesting.java
+++ src/main/java/com/epam/rd/autotasks/FactorialRegularInputTesting.java
@@ -1,7 +1,32 @@
+// FactorialRegularInputTesting.java
 package com.epam.rd.autotasks;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FactorialRegularInputTesting {
 
     Factorial factorial = new Factorial();
 
+    @Test
+    void testFactorialZero() {
+        assertEquals("1", factorial.factorial("0"));
+    }
+
+    @Test
+    void testFactorialOne() {
+        assertEquals("1", factorial.factorial("1"));
+    }
+
+    @Test
+    void testFactorialSmallNumber() {
+        assertEquals("120", factorial.factorial("5"));
+    }
+
+    @Test
+    void testFactorialLargeNumber() {
+        assertEquals("2432902008176640000", factorial.factorial("20"));
+    }
+
+    // Add more regular test cases as needed
 }
