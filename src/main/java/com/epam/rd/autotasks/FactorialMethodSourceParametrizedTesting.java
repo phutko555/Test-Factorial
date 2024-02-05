d9a983d9d5fb9a9c7380f5929dcd5dcd05d375e9
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-02-05 15:31:45 +0000

    Update FactorialMethodSourceParametrizedTesting.java

--- src/main/java/com/epam/rd/autotasks/FactorialMethodSourceParametrizedTesting.java
+++ src/main/java/com/epam/rd/autotasks/FactorialMethodSourceParametrizedTesting.java
@@ -1,7 +1,11 @@
+// FactorialMethodSourceParametrizedTesting.java
 package com.epam.rd.autotasks;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
 
 class FactorialMethodSourceParametrizedTesting {
 
@@ -10,7 +14,15 @@ class FactorialMethodSourceParametrizedTesting {
     @ParameterizedTest
     @MethodSource("testCases")
     void testFactorial(String in, String expected) {
-
+        assertEquals(expected, factorial.factorial(in));
     }
 
+    private static Stream<Object[]> testCases() {
+        return Stream.of(
+                new Object[]{"1", "1"},
+                new Object[]{"2", "2"},
+                new Object[]{"5", "120"}
+                // Add more test cases if needed
+        );
+    }
 }
